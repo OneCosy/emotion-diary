d1b7a591c233b6564fdb637c2e8a5297b1f0c474
한상진 <tkdwls6044@gmail.com>
2023-07-03 13:55:29 +0900

    New.js 코드 수정

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,12 +1,6 @@
 import { useEffect } from 'react';
 import DiaryEditor from "../components/DiaryEditor";
 
-const getStringDate = (date) => {
-    // toISOString() : YYYY-MM-DDTHH:mm:ss.sssZ로 반환해주기 때문에
-    // slice()를 이용해 0자리부터 9자리까지(YYYY - MM - DD) 잘라서 반환
-    return date.toISOString().slice(0, 10);
-};
-
 const New = () => {
 
     useEffect(() => {
